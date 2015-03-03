04cd809a774a70f6efe7f6868ec40439841eafc3
Vincent <vincent.frison@gmail.com>
2015-03-03 23:59:01 +0100

    New testing building with wrong computed area

--- misc/test_osm.sql
+++ misc/test_osm.sql
@@ -6,7 +6,7 @@ select count(distinct osm_id) from planet_osm_polygon;
 select count(*) from planet_osm_polygon where building <> '';
 select count(*) from planet_osm_polygon where building = 'yes';
 
-select osm_id, name, building, ST_AsEWKT(way) from planet_osm_polygon where building <> '' LIMIT 100;
+select osm_id, name, building, ST_AsEWKT(ST_Transform(way, 4326)) from planet_osm_polygon where building <> '' LIMIT 100;
 select osm_id, name, building, ST_AsEWKT(way) from planet_osm_polygon where building <> '' and name <> '' order by name LIMIT 100 ;
 
 select osm_id,name,building,ST_AsEWKT(way) from planet_osm_polygon where osm_id = 78158336;
@@ -16,3 +16,4 @@ where building <> '' and  ST_Contains(way, ST_Transform(ST_GeomFromText('POINT(2
 select osm_id,name,building,st_asewkt(way) from planet_osm_polygon 
 where building <> '' and  ST_Contains(way, ST_Transform(ST_GeomFromText('POINT(2.38475269926 48.8458926819)', 4326), 4326));
 
+603 916,511 127 149,595
