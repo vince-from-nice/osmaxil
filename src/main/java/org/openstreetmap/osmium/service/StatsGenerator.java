27c5c66904adcb6282408440e7128ab7de1a7e8c
Vincent <vincent.frison@gmail.com>
2015-03-29 13:46:26 +0200

    Adding basic statistics with bast matching score repartition

--- src/main/java/org/openstreetmap/osmium/service/StatsGenerator.java
+++ src/main/java/org/openstreetmap/osmium/service/StatsGenerator.java
@@ -2,19 +2,43 @@ package org.openstreetmap.osmium.service;
 
 import org.apache.log4j.Logger;
 import org.openstreetmap.osmium.Application;
+import org.openstreetmap.osmium.data.AbstractElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatsGenerator {
-    
+
+    private int[] elementsNbrByScoreRange;
+
     @Autowired
     private ElementCache elementCache;
 
     static private final Logger LOGGER = Logger.getLogger(Application.class);
-    
+
     public void displayStats() {
         LOGGER.info("=== Statistics ===");
-        LOGGER.info("TODO");
+        diplayStatsForScoreRanges();
+    }
+
+    private void diplayStatsForScoreRanges() {
+        LOGGER.info("Here is the repartition of best matching scores :");
+        makeStatsForScoreRanges();
+        for (int i = 0; i < 10; i++) {
+            LOGGER.info("- between " + i * 10 + "% and " + (i + 1) * 10 + "% : " + this.elementsNbrByScoreRange[i] + " ("
+                    + 100 * this.elementsNbrByScoreRange[i] / this.elementCache.getElements().size() + "%)");
+        }
+    }
+
+    private void makeStatsForScoreRanges() {
+        this.elementsNbrByScoreRange = new int[10];
+        for (AbstractElement element : this.elementCache.getElements().values()) {
+            for (int i = 0; i < 10; i++) {
+                if (element.getBestMatchingImport().getMatchingScore() < i * 0.1) {
+                    this.elementsNbrByScoreRange[i]++;
+                    break;
+                }
+            }
+        }
     }
 }
