4958dbaabc7ad45d14641457da5210e734d73aa7
Vincent Frison <vincent.frison@gmail.com>
2017-10-24 23:15:54 +0200

    Add an new elevation db loader for GeoTIFF files

---
+++ src/main/java/org/openstreetmap/osmaxil/plugin/loader/ElevationDbLoaderForTIFF.java
@@ -0,0 +1,51 @@
+package org.openstreetmap.osmaxil.plugin.loader;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service("ElevationDbLoaderForTIFF")
+@Lazy
+@Scope("prototype")
+public class ElevationDbLoaderForTIFF extends AbstractElevationDbLoader {
+
+	protected void loadData(String source) throws IOException {
+		File folder = new File(this.folderPath);
+		File[] files = folder.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".tiff") || name.toLowerCase().endsWith(".tif");
+			}
+		});
+		for (int i = 0; i < files.length; i++) {
+			File file = files[i];
+			LOGGER.info("Converting " + file + " to the XYZ format");
+			String xyzFilePath = file.getParentFile().getAbsolutePath() + File.separator + file.getName().substring(0, file.getName().length() - 4) + ".xyz";
+			String cmd = "gdal_translate.exe -of XYZ " + file.getAbsolutePath() + " " + xyzFilePath;
+			executeCommand(cmd);
+			LOGGER.info("Loading " + xyzFilePath);
+			this.copyPointCloudFromXYZFile(source, xyzFilePath);
+		}
+	}
+
+	public void loadData2(String source) {
+		LOGGER.info("Recreate the point cloud table from scratch.");
+		this.recreatePointCloudTable(source);
+		File xyzFolder = new File(this.folderPath);
+		File[] xyzFiles = xyzFolder.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".xyz");
+			}
+		});
+		for (int i = 0; i < xyzFiles.length; i++) {
+			File xyzFile = xyzFiles[i];
+			LOGGER.info("Loading file " + xyzFile);
+			this.copyPointCloudFromXYZFile(source, xyzFile.getAbsolutePath());
+		}
+		this.finalizePointCloudTable(source);
+	}
+
+}
